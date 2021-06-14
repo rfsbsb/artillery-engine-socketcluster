9459b4ef86a98c8973065a65e9c1ea59f2c6833a
semiaddict <info@semiaddict.com>
2021-06-14 15:38:22 +0200

    wip: implement sc specific methods

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 'use strict';
 
 const async = require('async');
@@ -23,16 +19,16 @@ module.exports = class SocketCusterEngine {
         return engineUtil.createThink(rs, _.get(this.config, 'defaults.think', {}));
       }
 
-      return this.step(rs, ee);
+      return this.createStep(rs, ee);
     });
 
     return this.compile(tasks, scenarioSpec.flow, ee);
   }
 
-  step(requestSpec, ee) {
+  createStep(requestSpec, ee) {
     if (requestSpec.loop) {
       let steps = _.map(requestSpec.loop, (rs) => {
-        return this.step(rs, ee);
+        return this.createStep(rs, ee);
       });
 
       return engineUtil.createLoopWithCount(
@@ -68,10 +64,12 @@ module.exports = class SocketCusterEngine {
       };
     }
 
-    const method = Object.keys(requestSpec)[0];
-    const fn = `sc_${method}`;
-    if (fn in this && _.isFunction(this[fn])) {
-      return this[fn].bind(this, requestSpec[method]);
+    const method = Object.keys(requestSpec).find((key) => {
+      const fn = `sc_${key}`;
+      return (fn in this && _.isFunction(this[fn]));
+    });
+    if (method) {
+      return this[`sc_${method}`].bind(this, requestSpec[method]);
     }
 
     return (context, callback) => {
@@ -80,10 +78,131 @@ module.exports = class SocketCusterEngine {
     }
   }
 
+  createClient(context, callback) {
+    const ee = context.ee;
+    const config = this.config;
+    const tls = config.tls || {};
+    const options = _.extend({
+      hostname: config.target
+    }, tls, config.socketcluster);
+    const auto_connect = !('autoConnect' in options) || options.autoConnect;
+
+    ee.emit('started');
+
+    options.autoConnect = false;
+    context.socket = socketclusterClient.create(options);
+
+    if (auto_connect) {
+      this.sc_connect({}, context, callback);
+    }
+    else {
+      callback(null, context);
+    }
+  }
+
+  compile(tasks, scenarioSpec, ee) {
+    return (initialContext, callback) => {
+      initialContext._successCount = 0;
+      initialContext.ee = ee;
+
+      let steps = _.flatten([
+        this.createClient.bind(this, initialContext),
+        tasks
+      ]);
+
+      async.waterfall(
+        steps,
+        (err, context) => {
+          if (err) {
+            debug(err);
+          }
+
+          this.sc_disconnect(context);
+
+          return callback(err, context);
+        });
+    };
+  }
+
+  capture(data, params, context, callback) {
+    debug('SC capture: %s', data);
+
+    if (!data) {
+      return callback(new Error('Empty response from SC server'), context);
+    }
+
+    let fauxResponse;
+    try {
+      fauxResponse = { body: JSON.parse(data) };
+    } catch (err) {
+      fauxResponse = { body: data }
+    }
+
+    engineUtil.captureOrMatch(
+      params,
+      fauxResponse,
+      context,
+      (err, result) => {
+        if (err) {
+          ee.emit('error', err.message || err.code);
+          return callback(err, context);
+        }
+
+        const { captures = {}, matches = {} } = result
+
+        debug('captures and matches:');
+        debug(matches);
+        debug(captures);
+
+        // match and capture are strict by default:
+        const haveFailedMatches = _.some(result.matches, (v, k) => {
+          return !v.success && v.strict !== false;
+        });
+
+        const haveFailedCaptures = _.some(result.captures, (v, k) => {
+          return v.failed;
+        });
+
+        if (haveFailedMatches || haveFailedCaptures) {
+          // TODO: Emit the details of each failed capture/match
+          return callback(new Error('Failed capture or match'), context);
+        }
+
+        _.each(result.matches, (v, k) => {
+          ee.emit('match', v.success, {
+            expected: v.expected,
+            got: v.got,
+            expression: v.expression,
+            strict: v.strict
+          });
+        });
+
+        _.each(result.captures, (v, k) => {
+          _.set(context.vars, k, v.value);
+        });
+
+        return callback(null, context);
+      }
+    )
+  }
+
+  invokeSCMethodWithoutCapture(method, params, context, callback, ...args) {
+    const ee = context.ee;
+
+    ee.emit('counter', `engine.socketcluster.${method}`, 1);
+    ee.emit('rate', `engine.socketcluster.${method}_rate`);
+
+    debug(`SC ${method}: %s`, JSON.stringify(params));
+
+    context.socket[method](...args);
+
+    return callback(null, context);
+  }
+
   sc_listener(params, context, callback) {
     return callback(null, context);
   }
-  
+
   sc_closeListener(params, context, callback) {
     return callback(null, context);
   }
@@ -141,221 +260,169 @@ module.exports = class SocketCusterEngine {
   }
 
   sc_transmit(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'transmit',
+      params, context, callback,
+      params.receiverName, params.data
+    );
   }
 
   sc_invoke(params, context, callback) {
-    return callback(null, context);
+    const ee = context.ee;
+
+    ee.emit('counter', 'engine.socketcluster.invoke', 1);
+    ee.emit('rate', 'engine.socketcluster.invoke_rate');
+
+    debug('SC invoke: %s', JSON.stringify(params));
+
+    (async () => {
+      context.socket.invoke(params.procedureName, params.data)
+        .then((result) => {
+          // only process response if we're capturing
+          if (params.capture) {
+            this.capture(result, params, context, callback);
+          }
+          else {
+            callback(null, context);
+          }
+        })
+        .catch((err) => {
+          callback(err, context);
+        });
+    })();
   }
 
   sc_send(params, context, callback) {
-    const ee = context.ee;
-
-    ee.emit('counter', 'engine.socketcluster.messages_sent', 1);
-    ee.emit('rate', 'engine.socketcluster.send_rate');
-
-    let startedAt = process.hrtime();
-
-    debug('SC send: %s', JSON.stringify(params));
-
-    const messageHandler = (event) => {
-      const { data } = event;
-
-      debug('SC receive: %s', data);
-
-      if (!data) {
-        return callback(new Error('Empty response from SC server'), context)
-      }
-
-      let fauxResponse;
-      try {
-        fauxResponse = { body: JSON.parse(data) };
-      } catch (err) {
-        fauxResponse = { body: event.data }
-      }
-
-      engineUtil.captureOrMatch(
-        params,
-        fauxResponse,
-        context,
-        (err, result) => {
-          if (err) {
-            ee.emit('error', err.message || err.code);
-            return callback(err, context);
-          }
-
-          const { captures = {}, matches = {} } = result
-
-          debug('captures and matches:');
-          debug(matches);
-          debug(captures);
-
-          // match and capture are strict by default:
-          const haveFailedMatches = _.some(result.matches, (v, k) => {
-            return !v.success && v.strict !== false;
-          });
-
-          const haveFailedCaptures = _.some(result.captures, (v, k) => {
-            return v.failed;
-          });
-
-          if (haveFailedMatches || haveFailedCaptures) {
-            // TODO: Emit the details of each failed capture/match
-            return callback(new Error('Failed capture or match'), context);
-          }
-
-          _.each(result.matches, (v, k) => {
-            ee.emit('match', v.success, {
-              expected: v.expected,
-              got: v.got,
-              expression: v.expression,
-              strict: v.strict
-            });
-          });
-
-          _.each(result.captures, (v, k) => {
-            _.set(context.vars, k, v.value);
-          });
-
-          return callback(null, context);
-        }
-      )
-    }
-
-    // only process response if we're capturing
-    if (params.capture) {
-      // Listen for messages
-      (async () => {
-        for await (let event of context.socket.listener('message')) {
-          messageHandler(event);
-        }
-      })();
-    }
-
-    context.socket.send(params.data, params.options || {});
+    return this.invokeSCMethodWithoutCapture(
+      'send',
+      params, context, callback,
+      params.data, params.options || {}
+    );
   }
 
   sc_authenticate(params, context, callback) {
-    return callback(null, context);
+    const ee = context.ee;
+
+    ee.emit('counter', 'engine.socketcluster.authenticate', 1);
+    ee.emit('rate', 'engine.socketcluster.authenticate_rate');
+
+    debug('SC authenticate: %s', JSON.stringify(params));
+
+    context.socket.authenticate(params.token)
+      .then(() => {
+        callback(null, context);
+      })
+      .catch((err) => {
+        callback(err, context);
+      });
   }
 
   sc_deauthenticate(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'deauthenticate',
+      params, context, callback
+    );
   }
 
   sc_transmitPublish(params, context, callback) {
+    const ee = context.ee;
+
+    ee.emit('counter', 'engine.socketcluster.transmitPublish', 1);
+    ee.emit('rate', 'engine.socketcluster.transmitPublish_rate');
+
+    debug('SC transmitPublish: %s', JSON.stringify(params));
+
+    context.socket.transmitPublish(params.channelName, params.data);
+
     return callback(null, context);
   }
 
   sc_invokePublish(params, context, callback) {
-    return callback(null, context);
+    const ee = context.ee;
+
+    ee.emit('counter', 'engine.socketcluster.invokePublish', 1);
+    ee.emit('rate', 'engine.socketcluster.invokePublish_rate');
+
+    debug('SC invokePublish: %s', JSON.stringify(params));
+
+    (async () => {
+      context.socket.invokePublish(params.channelName, params.data)
+        .then((result) => {
+          // only process response if we're capturing
+          if (params.capture) {
+            this.capture(result, params, context, callback);
+          }
+          else {
+            callback(null, context);
+          }
+        })
+        .catch((err) => {
+          callback(err, context);
+        });
+    })();
   }
 
   sc_subscribe(params, context, callback) {
     const ee = context.ee;
 
-    ee.emit('counter', 'engine.socketcluster.subscription', 1);
-    ee.emit('rate', 'engine.socketcluster.subscription_rate');
-
-    let startedAt = process.hrtime();
+    ee.emit('counter', 'engine.socketcluster.subscribe', 1);
+    ee.emit('rate', 'engine.socketcluster.subscribe_rate');
 
     debug('SC subscribe: %s', JSON.stringify(params));
-
-    const messageHandler = (event) => {
-      const { data } = event;
-
-      debug('SC receive: %s', data);
-
-      if (!data) {
-        return callback(new Error('Empty response from SC server'), context)
-      }
-
-      let fauxResponse;
-      try {
-        fauxResponse = { body: JSON.parse(data) };
-      } catch (err) {
-        fauxResponse = { body: event.data }
-      }
-
-      engineUtil.captureOrMatch(
-        params,
-        fauxResponse,
-        context,
-        (err, result) => {
-          if (err) {
-            ee.emit('error', err.message || err.code);
-            return callback(err, context);
-          }
-
-          const { captures = {}, matches = {} } = result
-
-          debug('captures and matches:');
-          debug(matches);
-          debug(captures);
-
-          // match and capture are strict by default:
-          const haveFailedMatches = _.some(result.matches, (v, k) => {
-            return !v.success && v.strict !== false;
-          });
-
-          const haveFailedCaptures = _.some(result.captures, (v, k) => {
-            return v.failed;
-          });
-
-          if (haveFailedMatches || haveFailedCaptures) {
-            // TODO: Emit the details of each failed capture/match
-            return callback(new Error('Failed capture or match'), context);
-          }
-
-          _.each(result.matches, (v, k) => {
-            ee.emit('match', v.success, {
-              expected: v.expected,
-              got: v.got,
-              expression: v.expression,
-              strict: v.strict
-            });
-          });
-
-          _.each(result.captures, (v, k) => {
-            _.set(context.vars, k, v.value);
-          });
-
-          return callback(null, context);
-        }
-      )
-    }
 
     const channel = context.socket.subscribe(params.channel, params.options || {});
 
     // only process response if we're capturing
     if (params.capture) {
-      // Listen for messages
+      // Capture data
       (async () => {
         for await (let data of channel) {
-          messageHandler(data);
+          this.capture(data, params, context, callback);
         }
       })();
+    }
+    else {
+      return callback(null, context);
     }
   }
 
   sc_unsubscribe(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'unsubscribe',
+      params, context, callback,
+      params.channel
+    );
   }
 
   sc_closeChannel(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'closeChannel',
+      params, context, callback,
+      params.channel
+    );
   }
 
   sc_channelCloseOutput(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'channelCloseOutput',
+      params, context, callback,
+      params.channel
+    );
   }
 
   sc_channelCloseAllListeners(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'channelCloseAllListeners',
+      params, context, callback,
+      params.channel
+    );
   }
 
   sc_closeAllChannels(params, context, callback) {
-    return callback(null, context);
+    return this.invokeSCMethodWithoutCapture(
+      'closeAllChannels',
+      params, context, callback
+    );
   }
 
   sc_killChannel(params, context, callback) {
@@ -382,17 +449,8 @@ module.exports = class SocketCusterEngine {
     return callback(null, context);
   }
 
-  connect(context, callback) {
-    const ee = context.ee;
-    const config = this.config;    
-    const tls = config.tls || {};
-    const options = _.extend({
-      hostname: this.config.target
-    }, tls, config.socketcluster);
-
-    ee.emit('started');
-
-    context.socket = socketclusterClient.create(options);
+  sc_connect(params, context, callback) {
+    context.socket.connect();
 
     // Listen for connection
     (async () => {
@@ -401,47 +459,21 @@ module.exports = class SocketCusterEngine {
         callback(null, context);
       }
     })();
-    
+
     // Listen for connection error once
     (async () => {
       for await (let event of context.socket.listener('connectAbort')) {
         context.socket.closeListener('connectAbort');
         debug(event);
         ee.emit('error', `${event.code}:${event.reason}`);
-        callback(event, {});
+        callback(event, null);
       }
     })();
   }
 
-  disconnect(context) {
+  sc_disconnect(context) {
     if (context && context.socket) {
       context.socket.disconnect();
     }
-  }
-
-  compile(tasks, scenarioSpec, ee) {
-    return (initialContext, callback) => {
-      let steps = _.flatten([
-        (callback) => {
-          initialContext._successCount = 0;
-          initialContext.ee = ee;
-          callback(null, initialContext);
-        },
-        this.connect.bind(this),
-        tasks
-      ]);
-
-      async.waterfall(
-        steps,
-        (err, context) => {
-          if (err) {
-            debug(err);
-          }
-
-          this.disconnect(context);
-
-          return callback(err, context);
-        });
-    };
   }
 }
